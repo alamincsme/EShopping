fb9d312f0dd3f646ba4c3fda0b068f2aac02c170
alamincsme <alamincsme@gmail.com>
2024-03-04 16:03:32 +0600

    E-Commerce using Spring boot , Spring security , my sql , hibernate

---
+++ src/main/java/io/alamincsme/EShoppersApplication.java
@@ -0,0 +1,13 @@
+package io.alamincsme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShoppersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EShoppersApplication.class, args);
+	}
+
+}
