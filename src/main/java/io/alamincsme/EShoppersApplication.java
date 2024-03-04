e33ab023075b6a260521c6e0f36529ca4a396f13
alamincsme <alamincsme@gmail.com>
2024-03-04 16:10:36 +0600

    e-Shoppers using spring boot , security , mysql, hibernate

--- src/main/java/io/alamincsme/EShoppersApplication.java
+++ src/main/java/io/alamincsme/EShoppersApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EShoppersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EShoppersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EShoppersApplication.class, args);
+    }
 
 }
--- src/test/java/io/alamincsme/EShoppersApplicationTests.java
+++ src/test/java/io/alamincsme/EShoppersApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EShoppersApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
