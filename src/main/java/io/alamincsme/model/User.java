b363fbf4c8ea08d010092390eb39a34fda99f1aa
alamincsme <alamincsme@gmail.com>
2024-03-13 17:20:47 +0600

    Create model user and address and they are relation

---
+++ src/main/java/io/alamincsme/model/User.java
@@ -0,0 +1,52 @@
+package io.alamincsme.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Size(min = 5, max = 20, message = "First Name must be between 5 and 30 characters long")
+    @Pattern(regexp = "^[a-zA-Z]*$", message = "First Name must not contain numbers or special characters")
+    private String firstName;
+
+    @Size(min = 5, max = 20, message = "First Name must be between 5 and 30 characters long")
+    @Pattern(regexp = "^[a-zA-Z]*$", message = "Last Name must not contain numbers or special characters")
+    private String lastName;
+
+    @Size(min = 11, max = 11, message = "Mobile Number must be exactly 11 digits long")
+    @Pattern(regexp = "^\\d{10}$", message = "Mobile Number must contain only Numbers")
+    private String mobileNumber;
+
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_address",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "address_id")
+    )
+    private List<Address> addresses = new ArrayList<>();
+
+
+}
