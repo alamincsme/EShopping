c7508875bc09767e38fe6e8fab57f1cb00b1763d
alamincsme <alamincsme@gmail.com>
2024-03-14 21:54:28 +0600

    Category Repository

---
+++ src/main/java/io/alamincsme/repositories/CategoryRepo.java
@@ -0,0 +1,9 @@
+package io.alamincsme.repositories;
+
+import io.alamincsme.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepo extends JpaRepository<Category, Long> {
+}
