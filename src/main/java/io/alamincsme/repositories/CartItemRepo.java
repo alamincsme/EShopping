160b36911b2adfcfd8dc63d26d3f7db27f67401c
alamincsme <alamincsme@gmail.com>
2024-03-14 21:53:22 +0600

    Cart Item Repository

---
+++ src/main/java/io/alamincsme/repositories/CartItemRepo.java
@@ -0,0 +1,9 @@
+package io.alamincsme.repositories;
+
+import io.alamincsme.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepo extends JpaRepository<CartItem, Long> {
+}
