eab99f1999dc6b1ef6d71545faebe352de643f2f
alamincsme <alamincsme@gmail.com>
2024-03-14 21:47:00 +0600

    User Repository

---
+++ src/main/java/io/alamincsme/repositories/UserRepo.java
@@ -0,0 +1,9 @@
+package io.alamincsme.repositories;
+
+import io.alamincsme.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+}
