def910563a9158d3ad320f580c0db021cdeb8ccd
Mohammad Alamin <alamincsme@gmail.com>
2024-03-20 10:38:02 +0600

    Product Service

--- src/main/java/io/alamincsme/service/ProductService.java
+++ src/main/java/io/alamincsme/service/ProductService.java
@@ -1,4 +1,16 @@
 package io.alamincsme.service;
 
+import io.alamincsme.model.Product;
+import io.alamincsme.payload.ProductDTO;
+import io.alamincsme.payload.ProductResponse;
+
 public interface ProductService {
+
+    ProductDTO addProduct(Long categoryId, Product product);
+    ProductResponse getAllProduct(Integer pageNo, Integer pageSize, String sortBy, String sortOrder);
+    ProductResponse searchByCategory(Long categoryId, Integer pageNo, Integer pageSize, String sortBy, String sortOrder);
+    ProductDTO updateProduct(Long productId, Product product) ;
+    ProductResponse searchProductByKeyword (String keyword , Integer pageNo, Integer pageSize, String sortBy, String sortOrder);
+    String deleteProduct (Long productId);
+
 }
