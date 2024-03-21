20e6b943e3dfcd3199dff753df52448196029187
Mohammad Alamin <alamincsme@gmail.com>
2024-03-21 15:37:36 +0600

    add getProductByCategory

--- src/main/java/io/alamincsme/controller/ProductController.java
+++ src/main/java/io/alamincsme/controller/ProductController.java
@@ -17,6 +17,10 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+    private Integer pageNo;
+    private Integer pageSize;
+    private String sortBy;
+    private String sortOrder;
 
     @PostMapping("/admin/categories/{categoryId}/product")
     public ResponseEntity<ProductDTO> addProduct(@Valid @RequestBody Product product, @PathVariable Long categoryId) {
@@ -38,5 +42,22 @@ public class ProductController {
 
         return new ResponseEntity<ProductResponse>(productResponse, HttpStatus.FOUND);
     }
+
+
+    @GetMapping("/public/categories/{categoryId}/products")
+    public ResponseEntity<ProductResponse> getProductByCategory(
+            @PathVariable(name = "categoryId") Long categoryId,
+            @RequestParam(name = "pageNo", defaultValue = AppConstants.PAGE_NUMBER, required = true) Integer pageNo,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = true) Integer pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_PRODUCTS_BY, required = true) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = true) String sortOrder) {
+
+        ProductResponse productResponse = productService.searchByCategory(categoryId, pageNo, pageSize, sortBy, sortOrder);
+
+        return new ResponseEntity<ProductResponse>(productResponse, HttpStatus.FOUND);
+
+    }
+
+
 
 }
