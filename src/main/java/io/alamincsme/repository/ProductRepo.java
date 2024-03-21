1ffcd90904d95afab0a49e1a043c7f9a8dbc8ce9
Mohammad Alamin <alamincsme@gmail.com>
2024-03-21 15:36:32 +0600

    add findByCategoryId method

--- src/main/java/io/alamincsme/repository/ProductRepo.java
+++ src/main/java/io/alamincsme/repository/ProductRepo.java
@@ -1,10 +1,13 @@
 package io.alamincsme.repository;
 
+import io.alamincsme.model.Category;
 import io.alamincsme.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
-
+    Page<Product> findByCategory(Category category, Pageable pageable);
 }
