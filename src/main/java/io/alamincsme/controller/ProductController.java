73ea23b4429830e7382e1be9cc9f8d8739cd776f
Mohammad Alamin <alamincsme@gmail.com>
2024-03-21 11:02:14 +0600

    Get All Product Controller method Implementation

--- src/main/java/io/alamincsme/controller/ProductController.java
+++ src/main/java/io/alamincsme/controller/ProductController.java
@@ -27,5 +27,16 @@ public class ProductController {
         return new ResponseEntity<ProductDTO>(savedProduct, HttpStatus.CREATED);
     }
 
+    @GetMapping("/public/products")
+    public ResponseEntity<ProductResponse> getAllProducts(
+            @RequestParam(name = "pageNo", defaultValue = AppConstants.PAGE_NUMBER, required = true) Integer pageNo,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = true) Integer pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_PRODUCTS_BY, required = true) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = true) String sortOrder) {
+
+        ProductResponse productResponse = productService.getAllProduct(pageNo, pageSize, sortBy, sortOrder);
+
+        return new ResponseEntity<ProductResponse>(productResponse, HttpStatus.FOUND);
+    }
 
 }
