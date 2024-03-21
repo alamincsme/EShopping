03aa126073d1f76151a55ec03c68c41ad6e902b2
Mohammad Alamin <alamincsme@gmail.com>
2024-03-22 04:54:51 +0600

    Cart Repo

--- src/main/java/io/alamincsme/repository/CartRepo.java
+++ src/main/java/io/alamincsme/repository/CartRepo.java
@@ -4,6 +4,9 @@ import io.alamincsme.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartRepo extends JpaRepository<Cart , Long> {
+
 }
