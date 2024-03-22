db7546880b214471a97ec63aa9dde44ca7bcd90c
Mohammad Alamin <alamincsme@gmail.com>
2024-03-22 09:58:27 +0600

    Category Service

--- src/main/java/io/alamincsme/service/CategoryService.java
+++ src/main/java/io/alamincsme/service/CategoryService.java
@@ -1,5 +1,15 @@
 package io.alamincsme.service;
 
+import io.alamincsme.model.Category;
+import io.alamincsme.payload.CategoryDTO;
+import io.alamincsme.payload.CategoryResponse;
+
 public interface CategoryService {
+
+    public CategoryDTO createCategory(Category category);
+    public CategoryResponse getCategories(Integer pageNo, Integer pageSize, String sortBy, String sortOrder);
+    public CategoryDTO updateCategory(Category category, Long categoryId);
+    public String deleteCategory(Long categoryId);
+
 
 }
