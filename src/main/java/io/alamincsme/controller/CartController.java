edbe873c8543856bd1f4b9c139baabb272034fd3
Mohammad Alamin <alamincsme@gmail.com>
2024-03-23 21:11:43 +0600

    Add Product to cart in cart controller

---
+++ src/main/java/io/alamincsme/controller/CartController.java
@@ -0,0 +1,30 @@
+package io.alamincsme.controller;
+
+import io.alamincsme.payload.CartDTO;
+import io.alamincsme.payload.ProductDTO;
+import io.alamincsme.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CartController {
+
+    @Autowired
+    private CartService cartService ;
+
+    @PostMapping("/public/carts/{cartId}/products/{productId}/quantity/{quantity}")
+    public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long cartId,
+                                                    @PathVariable Long productId,
+                                                    @PathVariable Integer quantity){
+
+        var cartDTO = cartService.addProductToCart(cartId, productId, quantity);
+
+        return new  ResponseEntity<CartDTO> (cartDTO,HttpStatus.CREATED);
+
+    }
+}
