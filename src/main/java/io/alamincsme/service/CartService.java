41ad4998ae9ba484b72936207d0bdb153d871123
Mohammad Alamin <alamincsme@gmail.com>
2024-03-23 21:16:32 +0600

    Cart Service

--- src/main/java/io/alamincsme/service/CartService.java
+++ src/main/java/io/alamincsme/service/CartService.java
@@ -1,4 +1,17 @@
 package io.alamincsme.service;
 
+import com.mysql.cj.log.Log;
+import io.alamincsme.payload.CartDTO;
+
+import java.util.List;
+
 public interface CartService {
+    CartDTO addProductToCart(Long cartId, Long productId, Integer quantity);
+    List<CartDTO> getAllCarts();
+    CartDTO getCart(String emailId, Long cartId) ;
+    CartDTO updateProductQuantityInCart(Long cartId, Long productId, Integer quantity) ;
+    void updateProductInCarts(Long cartId, Long productId) ;
+    String deleteProductFromCart(Long cartid, Long productId);
+
+
 }
