fafc5e46fad62329e0ac6f678b617259e7c33a56
Mohammad Alamin <alamincsme@gmail.com>
2024-03-23 21:12:50 +0600

    Add product ot cart in Product Service Impl

---
+++ src/main/java/io/alamincsme/service/CartServiceImp.java
@@ -0,0 +1,108 @@
+package io.alamincsme.service;
+
+import io.alamincsme.exception.APIException;
+import io.alamincsme.exception.ResourceNotFoundException;
+import io.alamincsme.model.Cart;
+import io.alamincsme.model.CartItem;
+import io.alamincsme.model.Product;
+import io.alamincsme.payload.CartDTO;
+import io.alamincsme.payload.ProductDTO;
+import io.alamincsme.repository.CartItemRepo;
+import io.alamincsme.repository.CartRepo;
+import io.alamincsme.repository.ProductRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CartServiceImp implements CartService {
+    @Autowired
+    private CartRepo cartRepo ;
+
+    @Autowired
+    private CartItemRepo cartItemRepo;
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    @Override
+    public CartDTO addProductToCart(Long cartId, Long productId, Integer quantity) {
+        Cart cart = cartRepo.findById(cartId)
+                .orElseThrow(() -> new ResourceNotFoundException("Cart", "cartId", cartId));
+        Product product = productRepo.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
+
+
+        CartItem cartItem = cartItemRepo.findCartItemByProductIdAndCartId(cartId, productId);
+
+        if (cartItem != null) {
+            throw new APIException("Product " + product.getProductName() + " already exists in the cart");
+        }
+
+        if (product.getQuantity() == 0) {
+            throw new APIException(product.getProducts() + " is not available");
+        }
+
+        if (product.getQuantity() < quantity) {
+            throw  new APIException("Please! make an order of the " + product.getProductName() + " less than or equal ot the quantity " + product.getQuantity());
+        }
+
+        CartItem newCartItem = new CartItem();
+        newCartItem.setProduct(product);
+        newCartItem.setCart(cart);
+        newCartItem.setQuantity(quantity);
+        newCartItem.setDiscount(product.getDiscount());
+        newCartItem.setProductPrice(product.getSpecialPrice());
+
+        cartItemRepo.save(newCartItem);
+
+        product.setQuantity(product.getQuantity() - quantity);
+
+        cart.setTotalPrice(cart.getTotalPrice()  + product.getSpecialPrice() * quantity);
+
+        CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
+        List<ProductDTO> productDTOS = cart.getCartItems()
+                                            .stream()
+                                            .map(p -> modelMapper.map(p.getProduct(), ProductDTO.class))
+                                            .collect(Collectors.toList());
+
+//        for (ProductDTO productDTO : productDTOS) System.out.println(productDTO.getDescription());
+        cartDTO.setProducts(productDTOS);
+        return cartDTO ;
+
+    }
+
+
+
+    @Override
+    public List<CartDTO> getAllCarts() {
+        return null;
+    }
+
+    @Override
+    public CartDTO getCart(String emailId, Long cartId) {
+        return null;
+    }
+
+    @Override
+    public CartDTO updateProductQuantityInCart(Long cartId, Long productId, Integer quantity) {
+        return null;
+    }
+
+    @Override
+    public void updateProductInCarts(Long cartId, Long productId) {
+
+    }
+
+    @Override
+    public String deleteProductFromCart(Long cartid, Long productId) {
+        return null;
+    }
+}
