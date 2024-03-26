f61bfda49523b5ef4a6b5ba27d82de5a87156386
Mohammad Alamin <alamincsme@gmail.com>
2024-03-26 10:21:21 +0600

    Cart Repo

--- src/main/java/io/alamincsme/repository/CartRepo.java
+++ src/main/java/io/alamincsme/repository/CartRepo.java
@@ -2,11 +2,14 @@ package io.alamincsme.repository;
 
 import io.alamincsme.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CartRepo extends JpaRepository<Cart , Long> {
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1 AND c.cartId = ?2")
+    Cart findCartByEmailAndCartId(String emailId, Long cartId);
 
 }
