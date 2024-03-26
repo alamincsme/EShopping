c5c67ef9de9764f9e47d01a8744439f7610b6c51
Mohammad Alamin <alamincsme@gmail.com>
2024-03-26 10:23:39 +0600

    Update cart item service

--- src/main/java/io/alamincsme/repository/CartItemRepo.java
+++ src/main/java/io/alamincsme/repository/CartItemRepo.java
@@ -1,7 +1,7 @@
 package io.alamincsme.repository;
 
 import io.alamincsme.model.CartItem;
-import io.alamincsme.model.Product;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Repository;
 public interface CartItemRepo extends JpaRepository<CartItem, Long>{
     @Query("SELECT ci FROM CartItem ci WHERE ci.cart.cartId = ?1 AND ci.product.productId = ?2")
     CartItem findCartItemByProductIdAndCartId(Long cartId, Long productId);
-    
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM CartItem ci WHERE ci.cart.cartId = ?1 AND ci.product.productId = ?2")
+    void deleteCartItemByProductIdAndCartId(Long cartId, Long productId);
 
 }
