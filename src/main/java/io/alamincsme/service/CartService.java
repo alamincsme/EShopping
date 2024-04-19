46271a68dd43cabab3f50e396570ac05c97a50c9
Mohammad Alamin <alamincsme@gmail.com>
2024-04-19 22:55:28 +0600

    Cart Service

--- src/main/java/io/alamincsme/service/CartService.java
+++ src/main/java/io/alamincsme/service/CartService.java
@@ -6,11 +6,10 @@ import io.alamincsme.payload.CartDTO;
 import java.util.List;
 
 public interface CartService {
-    CartDTO addProductToCart(Long cartId, Long productId, Integer quantity);
-    List<CartDTO> getAllCarts();
-    CartDTO getCart(String emailId, Long cartId) ;
+//    CartDTO addProductToCart(Long cartId, Long productId, Integer quantity);
+//    List<CartDTO> getAllCarts();
+//    CartDTO getCart(String emailId, Long cartId) ;
     CartDTO updateProductQuantityInCart(Long cartId, Long productId, Integer quantity) ;
-    void updateProductInCarts(Long cartId, Long productId) ;
     String deleteProductFromCart(Long cartid, Long productId);
 
 
