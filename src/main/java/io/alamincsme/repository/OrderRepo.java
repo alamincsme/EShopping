b964a904ef49e63b38b88280d58966de5cfb5401
Mohammad Alamin <alamincsme@gmail.com>
2024-04-21 01:27:22 +0600

    Order Repo

--- src/main/java/io/alamincsme/repository/OrderRepo.java
+++ src/main/java/io/alamincsme/repository/OrderRepo.java
@@ -4,7 +4,9 @@ import io.alamincsme.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepo extends JpaRepository<Order, Long> {
-
+    List<Order> findAllByEmail(String emailId);
 }
