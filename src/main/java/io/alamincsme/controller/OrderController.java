30c26a3de2e4e0f520226a1b325df4f38fd77b98
Mohammad Alamin <alamincsme@gmail.com>
2024-04-20 12:52:06 +0600

    Order controller

---
+++ src/main/java/io/alamincsme/controller/OrderController.java
@@ -0,0 +1,31 @@
+package io.alamincsme.controller;
+
+import io.alamincsme.payload.OrderDTO;
+import io.alamincsme.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+
+    @Autowired
+    private  OrderService orderService;
+
+    @PostMapping("/user/{emailId}/cart/{cartId}/payments/{paymentMethod}/order")
+    public ResponseEntity<OrderDTO> processOrder(@PathVariable String emailId, @PathVariable Long cartId,@PathVariable String paymentMethod) {
+        return new ResponseEntity<>(orderService.placeOrder(emailId, cartId, paymentMethod), HttpStatus.CREATED);
+    }
+
+
+
+
+
+
+
+}
