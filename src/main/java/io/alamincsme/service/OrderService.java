d90d196684fb4ca08a439f87d9aa970c48a98524
Mohammad Alamin <alamincsme@gmail.com>
2024-04-21 04:41:49 +0600

    Order Service

--- src/main/java/io/alamincsme/service/OrderService.java
+++ src/main/java/io/alamincsme/service/OrderService.java
@@ -2,6 +2,12 @@ package io.alamincsme.service;
 
 import io.alamincsme.payload.OrderDTO;
 
+import java.util.List;
+
 public interface OrderService {
     OrderDTO placeOrder(String emailId, Long cartId, String paymentMethod);
+    List<OrderDTO> getOrdersByUser(String emailId);
+    OrderDTO getOrder(String emailId, Long orderId);
+
+
 }
