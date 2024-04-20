c6779caf73a7661bf1880d36cc33675e1c2c57f0
Mohammad Alamin <alamincsme@gmail.com>
2024-04-21 05:00:37 +0600

    Order Repository

--- src/main/java/io/alamincsme/repository/OrderRepo.java
+++ src/main/java/io/alamincsme/repository/OrderRepo.java
@@ -2,6 +2,7 @@ package io.alamincsme.repository;
 
 import io.alamincsme.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ import java.util.List;
 @Repository
 public interface OrderRepo extends JpaRepository<Order, Long> {
     List<Order> findAllByEmail(String emailId);
+
+    @Query("SELECT o FROM Order  o WHERE o.email=?1 AND o.orderId=?2")
+    Order findOrderByEmailAndOrderId(String emailId, Long orderId);
 }
