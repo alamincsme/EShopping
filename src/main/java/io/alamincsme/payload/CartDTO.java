a6227b3e33be4c73f2a6dfc1087bb741e34b90c3
Mohammad Alamin <alamincsme@gmail.com>
2024-04-20 09:03:06 +0600

    Cart DTO

--- src/main/java/io/alamincsme/payload/CartDTO.java
+++ src/main/java/io/alamincsme/payload/CartDTO.java
@@ -14,5 +14,6 @@ public class CartDTO {
 
     private Long cartId;
     private Double totalPrice = 0.0;
-    private List<ProductDTO> products = new ArrayList<>();
+    private List<CartItemDTO> cartItemDTOs = new ArrayList<>();
+
 }
