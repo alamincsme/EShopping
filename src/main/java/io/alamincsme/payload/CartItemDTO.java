fcb5cf930a24b8bd50dca8fb18f5f61b8dac7945
Mohammad Alamin <alamincsme@gmail.com>
2024-04-20 09:03:40 +0600

    Cart Item DTO

--- src/main/java/io/alamincsme/payload/CartItemDTO.java
+++ src/main/java/io/alamincsme/payload/CartItemDTO.java
@@ -3,16 +3,16 @@ package io.alamincsme.payload;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CartItemDTO {
 
-    private Long cartItemId;
-    private CartDTO cart;
     private ProductDTO product;
     private Integer quantity;
     private double discount;
     private double productPrice;
+
 }
